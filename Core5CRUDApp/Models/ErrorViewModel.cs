57fa7de0fb707f7044f44a57b1e79c3683b7397a
Dewanto Krisna <dewanto.krisna@gmail.com>
2021-01-29 04:31:29 +0700

    initialize New CORE 5 PROJ

---
+++ Core5CRUDApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Core5CRUDApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
